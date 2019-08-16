1f27a29d905293c10524dc0baec9e337c1664045
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-08-16 11:28:13 -0500

    submit exam

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 3262 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,77 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  fetch(imageURL)
+  .then(resp => resp.json())
+  .then(data => {
+    loadImage(data)
+
+    const likeBtn = document.getElementById('like_button')
+    likeBtn.addEventListener('click', (event) => addLikes(data))
+    
+
+    const sub = document.getElementById('comment_form')
+    sub.addEventListener('submit', (event) => addComments(event, data))
+
+  })
+
 })
+
+     const img = document.getElementById('image');
+     const name = document.getElementById('name');
+     const like = document.getElementById('likes');
+     const coms = document.getElementById('comments');
+
+function loadImage(elem){
+     
+     img.src = elem.url
+     name.innerText = elem.name
+     like.innerText = elem.like_count
+     elem.comments.forEach(item => {
+       const com = document.createElement('li')
+       com.innerText = item.content
+       coms.appendChild(com)
+     })
+    }
+
+
+    function addLikes(param){
+      
+      fetch(`https://randopic.herokuapp.com/likes/`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          image_id: param.id
+        })
+      })
+      .then(resp => resp.json())
+      
+    }
+
+
+    
+
+    function addComments(event, item){
+      event.preventDefault()
+      let cont = document.getElementById('comment_input')
+
+      fetch(`https://randopic.herokuapp.com/comments/`, {
+
+         method: 'POST',
+         headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          image_id: item.id,
+          content: cont.value
+        })
+      })
+      .then(resp => resp.json())
+
+    }
+
+
